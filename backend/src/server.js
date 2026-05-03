c4de236328e05e5e4169e3a495a8a496ade41708
Luiz Miguel <luiz.miguel.sebastiani@gmail.com>
2026-05-03 15:53:38 -0300

    ajust dockfiler

--- backend/src/server.js
+++ backend/src/server.js
@@ -35,7 +35,7 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0',() => {
   console.log(`🚀 Servidor rodando na porta ${PORT}`);
   console.log(`📡 API disponível em http://localhost:${PORT}/api`);
 });
